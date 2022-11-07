5831d96a9d5a251a8aee61370ac3195a618c1c44
Chansik Yun <chansigi@gmail.com>
2022-11-07 15:11:29 +0900

    .

--- 1_전처리기7.c
+++ 1_전처리기7.c
@@ -2,17 +2,6 @@
 
 // 매크로 함수를 인라인 함수로 대체할 수 있다면, 인라인 함수가 선호되어야 한다.
 
-#include <stdio.h>
-
-// 예외 1. 매크로 함수를 사용해서, 토큰을 연결하거나, 문자열 변경 기능을 사용할 때
-
-enum Color
-{
-  Color_Red,
-  Color_Blue,
-  Color_Green
-};
-
 #if 0
 struct color
 {
@@ -24,6 +13,17 @@ struct color
     {Color_Blue, "Blue"},
 };
 #endif
+
+// 예외 1. 매크로 함수를 사용해서, 토큰을 연결하거나, 문자열 변경 기능을 사용할 때
+#if 0
+#include <stdio.h>
+
+enum Color
+{
+  Color_Red,
+  Color_Blue,
+  Color_Green
+};
 
 #define COLOR(color)      \
   {                       \
@@ -46,6 +46,28 @@ int main(void)
   {
     printf("%d %s\n", colors[i].color, colors[i].name);
   }
+
+  return 0;
+}
+#endif
+
+#include <stdio.h>
+
+// #, ##
+#define TO_STR(str) #str
+#define CONCAT(a, b) a##b
+
+int main(void)
+{
+  int n1 = 10;
+  int n2 = 20;
+  int n3 = 30;
+
+  printf("%d %d %d\n", CONCAT(n, 1), CONCAT(n, 2), CONCAT(n, 3));
+  //                    n##1 => n1     n##2 => n2   n##3 => n3
+
+  printf("%s\n", TO_STR(hello));
+  //              #hello => "hello"
 
   return 0;
 }
