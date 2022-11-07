b35c18f68ba9737bb8d2d4593b4cb9062c197a3e
Chansik Yun <chansigi@gmail.com>
2022-11-07 13:21:25 +0900

    .

--- 1_전처리기4.c
+++ 1_전처리기4.c
@@ -34,7 +34,8 @@ int main(void)
 }
 #endif
 
-// 해결 방법
+#if 0
+// 해결 방법 1.
 // 1. 매크로 함수의 매개 변수에 대해서 증가 또는 감소를 불완전한 매크로 함수
 //    이전에 수행해야 합니다.
 // 2. 매크로 함수의 이름에 안전하지 않음을 알립니다.
@@ -50,6 +51,54 @@ int main(void)
   int a = -10;
   --a;
   printf("%d\n", UNSAFE_ABS(a));
+
+  return 0;
+}
+#endif
+
+// 해결 방법 2.
+//  => C99 표준에서 새롭게 도입된 문법이 있습니다.
+//  => inline 함수
+//  C++
+//   inline void foo(void) {}
+//  C
+//   static inline void foo(void) {}
+
+//  => 인라인 함수는 함수 호출을 수행하지 않고, 함수 본문으로 기계어 치환을
+//     수행합니다.
+//  => 인라인 최적화는 컴파일 최적화 명령입니다.
+//     사용자가 inline 키워드를 사용해도, 무조건 인라인화가 수행되지 않습니다.
+//     MSVC: /Ob1
+
+static inline int square(int x) { return x * x; }
+static inline int abs(int x) { return x > 0 ? x : -x; }
+
+#define SQR(x) ((x) * (x))
+#define ABS(x) ((x) > 0 ? (x) : -(x))
+
+int main(void)
+{
+  int x = 2;
+  printf("%d\n", square(++x));
+  /*
+          mov     eax, DWORD PTR x$[rsp]
+          imul    eax, DWORD PTR x$[rsp]
+          mov     edx, eax
+          lea     rcx, OFFSET FLAT:$SG6987
+          call    printf
+  */
+  printf("%d\n", SQR(x));
+  /*
+          mov     eax, DWORD PTR x$[rsp]
+          imul    eax, DWORD PTR x$[rsp]
+          mov     edx, eax
+          lea     rcx, OFFSET FLAT:$SG6988
+          call    printf
+  */
+
+  int a = -10;
+  printf("%d\n", abs(--a));
+  printf("%d\n", ABS(a));
 
   return 0;
 }
