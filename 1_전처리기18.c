6cd1b7089fd6c0ef353a477df9b758fc7a262c32
Chansik Yun <chansigi@gmail.com>
2022-11-08 11:44:56 +0900

    .

--- 1_전처리기18.c
+++ 1_전처리기18.c
@@ -23,13 +23,28 @@ void foo
 #define CALL_FOO(a, b, c) foo(a, b, c)
 #endif
 
+//--------------------
+// => 매크로 함수의 인자 목록에서 전처리기 지시어에 대한 사용은 미정의 동작입니다.
+//   : C 표준 라이브러리의 많은 함수가 매크로로 제공될 수 있습니다.
+//     - memcpy, printf, assert
+
 int main(void)
 {
+  /* 잘못된 코드 */
+  /*
   CALL_FOO(10, 20,
 #if (MORE_ARGS == 1)
            30,
 #endif
            40);
+  */
+
+  /* 해결 방법 */
+#if (MORE_ARGS == 1)
+  CALL_FOO(10, 20, 30, 40);
+#else
+  CALL_FOO(10, 20, 40);
+#endif
 
   return 0;
 }
