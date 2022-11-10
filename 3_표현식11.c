712b7d644d1530bc186fd7ded7c15a49994d2c37
Chansik Yun <chansigi@gmail.com>
2022-11-11 08:41:41 +0900

    .

--- 3_표현식11.c
+++ 3_표현식11.c
@@ -7,12 +7,14 @@ void func(int a, int b)
   printf("%d %d\n", a, b);
 }
 
-
 int main(void)
 {
   int i = 2;
 
-  func(i++, i); /* 미정의 동작 */
+  // func(i++, i); /* 미정의 동작 */
+
+  func(i, i);
+  ++i;
 
   return 0;
 }
@@ -36,7 +38,6 @@ int f2(void)
   global = 100;
   return 20;
 }
-
 
 int main(void)
 {
@@ -111,6 +112,7 @@ int main(void)
   struct AAA aaa = {foo};
   aaa.f(NULL); // foo
 
+  /* 해결 방법 */
   struct AAA *r = g(&aaa);
   aaa.f(r); // goo
 
@@ -145,11 +147,9 @@ int main(void)
   printf("r1: %d\n", r1);
   printf("global: %d\n", global);
 
-  int r2 = f1() && f2(); /* OK, f1() -> f2() 표준 지정되어 있습니다. */
-  int r3 = f1() || f2(); /* OK */
-
-  int r4 = (f1(), f2()); /* OK, 나열 연산자도 순서가 지정되어 있습니다. */
-
+  int r2 = f1() && f2();    /* OK, f1() -> f2() 표준 지정되어 있습니다. */
+  int r3 = f1() || f2();    /* OK */
+  int r4 = (f1(), f2());    /* OK, 나열 연산자도 순서가 지정되어 있습니다. */
   int r5 = f1() ? f2() : 0; /* OK, 삼항 연산자도 순서가 지정되어 있습니다. */
 
   return 0;
