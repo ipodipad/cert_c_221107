5d1490f052ea012d1c100dc8bd71c1b3482ca692
Chansik Yun <chansigi@gmail.com>
2022-11-10 10:21:24 +0900

    .

--- 2_선언22.c
+++ 2_선언22.c
@@ -8,7 +8,7 @@
 //     있습니다.
 //  주의: 상수 인수를 전달할 경우, 반환되는 포인터를 통해 상수 메모리를 수정할 경우
 //      미정의 동작이 발생합니다.
-
+#if 0
 int main(void)
 {
   // char *s = "hello";
@@ -30,6 +30,22 @@ int main(void)
     *p2 = 'x';
     printf("%s\n", s2);
   }
+
+  return 0;
+}
+#endif
+
+// 수정이 불가능한 이전에 정의되어 있는 레거시 함수입니다.
+void legacy_log(char *msg)
+{
+  fprintf(stderr, "Err: %s\n", msg);
+}
+
+int main(void)
+{
+  const char *INVFNAME = "Invalid file name";
+  legacy_log((char *)INVFNAME);
+  // 타입의 불일치 문제를 해결하기 위해서만 캐스팅을 이용해야 합니다.
 
   return 0;
 }
