056f198ad5a1b4f1001399334b30a01f44e07d5d
Chansik Yun <chansigi@gmail.com>
2022-11-10 15:33:47 +0900

    .

--- 3_표현식11.c
+++ 3_표현식11.c
@@ -84,6 +84,7 @@ int main(void)
 }
 #endif
 
+#if 0
 struct AAA
 {
   void (*f)(struct AAA *p);
@@ -118,8 +119,9 @@ int main(void)
   aaa.f = foo;
   aaa.f(g(&aaa)); /* 미지정 동작 */
 }
+#endif
 
-#if 0
+#if 1
 int global = 0;
 int f1(void)
 {
@@ -135,13 +137,20 @@ int f2(void)
 
 int main(void)
 {
-  // int r1 = f1() + f2(); /* 미지정 동작 */
+  // int r1 = f1() + f2(); /* 이항 연산자(+, -, *, /) - 미지정 동작 */
   int r_f1 = f1();
   int r_f2 = f2();
   int r1 = r_f1 + r_f2;
 
   printf("r1: %d\n", r1);
   printf("global: %d\n", global);
+
+  int r2 = f1() && f2(); /* OK, f1() -> f2() 표준 지정되어 있습니다. */
+  int r3 = f1() || f2(); /* OK */
+
+  int r4 = (f1(), f2()); /* OK, 나열 연산자도 순서가 지정되어 있습니다. */
+
+  int r5 = f1() ? f2() : 0; /* OK, 삼항 연산자도 순서가 지정되어 있습니다. */
 
   return 0;
 }
