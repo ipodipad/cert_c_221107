8586966137858887c7c3b16cab2ac2c6bc6cc91e
Chansik Yun <chansigi@gmail.com>
2022-11-10 09:50:36 +0900

    .

--- 2_선언21.c
+++ 2_선언21.c
@@ -121,7 +121,14 @@ int main(void)
 #endif
 
 // 2. 스레드마다 독립적인 정적 변수
-//  => TLS(Thread Local Stroage)
+//  => TLS(Thread Local Stroage) / TSD(Thread Specific Data)
+//   1) 정적 TLS
+//    => __thread int cnt = 0;
+//   2) 동적 TLS
+//    => 스레드 함수
+//       pthread_setspecific
+//       pthread_getspecific
+
 __thread int cnt = 0;
 
 void *foo(void *arg)
@@ -153,3 +160,9 @@ int main(void)
 
   return 0;
 }
+
+// C11에 스레드 API가 표준으로 도입되었습니다.
+// => 더이상 플랫폼 종속적인 API를 이용해서 스레드를 만들 필요가 없습니다.
+// #include <threads.h> // C11
+
+// #include <thread> // C++11
